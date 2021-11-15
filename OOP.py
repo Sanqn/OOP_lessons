2806a68cd11d513124955567af2fcf8e978f32ca
Sanqn <sandisvert@gmail.com>
2021-11-15 21:52:06 +0300

    class_Counter

--- OOP.py
+++ OOP.py
@@ -1,6 +1,83 @@
-class Cat:
-    name = 'Матроскин'
-    color = 'black'
+# class Cat:
+#     name = 'Micki'
+#     color = 'black'
 
+# print(Cat.name)
+# print(Cat.__dict__)
+# print(getattr(Cat, 'color')) #black
+# print(getattr(Cat, 'age', 5))
+# Cat.age = 10 #append 'age': 10
+# setattr(Cat, 'price', 100) #append 'price': 100
+# print(Cat.__dict__)
+# del Cat.age #'age': 10
+# print(Cat.__dict__)
+# delattr(Cat, 'price') # del 'price': 100
+# print(Cat.__dict__)
+#
+# a = Cat()
+# b = Cat()
+# Cat.age = 10
+# a.surname = 'joom'
+# print(a.surname)
 
-my_cat = Cat()
+# class Car:
+#     model = "BMW"
+#     engine = 1.6
+#
+#     @staticmethod
+#     def f():
+#         print('Hello world')
+#
+# Car.f()
+# print(Car.engine)
+# a = Car()
+# print(a.f())
+
+# class Cat:
+#     breed = 'Pers'
+#     def set_value(self, name, age = 0):
+#         self.name = name
+#         self.age = age
+#
+#     def hello_cat(self):
+#         print('Hello', self.name)
+#
+#     def say_how_are_you(self):
+#         print('How are you', self.name)
+#
+# jerry = Cat()
+# jerry.name = 'Jarry'
+# jerry.age = 5
+# jerry.breed = 'Seam'
+# print(jerry.__dict__)
+# jerry.hello_cat()
+# jerry.say_how_are_you()
+
+# class Lion:
+#     def roar(self):
+#         print('Rrrrrrr!!!')
+# sinba = Lion()
+# sinba.roar()
+
+class Counter:
+    def start_from(self, start = 0):
+        self.start = start
+
+    def increment(self):
+        self.start += 1
+
+    def display(self):
+        print(f'Текущее значение счетчика = {self.start}')
+
+    def reset(self):
+        self.start = 0
+
+c1 = Counter()
+c1.start_from()
+c1.increment()
+c1.display() # печатает "Текущее значение счетчика = 1"
+c1.increment()
+c1.display() # печатает "Текущее значение счетчика = 2"
+c1.reset()
+c1.display() # печатает "Текущее значение счетчика = 0"
+
